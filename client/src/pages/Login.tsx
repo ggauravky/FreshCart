d1c642ca99a0da1911714a1a712dfd70631448c2
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-19 20:17:27 +0530

    create login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,121 @@
+import { useState } from "react"
+import heroBg from "../assets/hero_bg.jpeg"
+import { Link } from "react-router-dom"
+import { BikeIcon, LockIcon, UserIcon , MailIcon, Loader2Icon } from "lucide-react"
 
 
-const login = () => {
+const Login = () => {
+  const [isLoginState, setIsLoginState] = useState(true)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e: React.SubmitEvent) => {
+    e.preventDefault()
+    setLoading(true)
+    setTimeout(() => {
+      window.location.href = "/"
+    }, 1000)
+  }
+
   return (
-    <div>login</div>
+    <div className="min-h-screen flex">
+      <div className="hidden lg:flex lg:w-1/2 bg-app-green relative items-center justify-center">
+        <img
+          src={heroBg}
+          alt="hero"
+          className="absolute inset-0 h-full object-cover bg-center opacity-10"
+        />
+        <div className="relative text-center px-12">
+          <h2 className="text-4xl font-semibold text-white mb-4">
+            Welcome to FreshCart
+          </h2>
+          <p className="text-white/60 font-serif text-xl max-w-sm mx-auto">
+            Discover the best products at unbeatable prices.
+          </p>
+        </div>
+      </div>
+      {/* LRight Side */}
+      <div className="flex-1 flex flex-center px-4 py-12 bg-app-cream">
+        <div className="w-full max-w-md">
+          {/* {form header message} */}
+          <div className="text-center mb-8">
+            <Link to="/" className="inline-flex items-center gap-2 mb-6">
+              <BikeIcon className="size-8 text-app-green" />
+              <span className="text-2xl font-semibold text-app-green">FreshCart</span>
+            </Link>
+            <h1 className="text-2xl font-semibold text-app-green mb-2">
+              {isLoginState ? "Sign In to your account" : "Sign Up for a new account"}
+            </h1>
+            <p className="text-sm text-app-text-light">
+              {isLoginState ? "Don't have an account?" : "Already have an account?"} 
+              <button onClick={()=>setIsLoginState(!isLoginState)}
+                className="text-orange-500 ml-1 font-semibold hover:text-orange-600 transition-colors">
+                {isLoginState ? "Create one" : "Sign in"}
+              </button>
+            </p>
+          </div>
+
+          {/* "login / Register form" */}
+          <form onSubmit={handleSubmit} className="space-y-5">
+          {!isLoginState && (
+            <label className="text-sm flex flex-col gap-1">
+              Name
+              <div className="relative">
+                <UserIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  value={name}
+                  required
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                />
+              </div>
+            </label>
+          )}
+          <label className="text-sm flex flex-col gap-1">
+              Email
+              <div className="relative">
+                <MailIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+                <input
+                  type="email"
+                  placeholder="you@example.com"
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                />
+              </div>
+            </label>
+            <label className="text-sm flex flex-col gap-1">
+              Password
+              <div className="relative">
+                <LockIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light" />
+                <input
+                  type="password"
+                  placeholder="******"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                />
+              </div>
+            </label>
+
+          <button
+              type="submit"
+              disabled={loading}
+              className="flex-center w-full py-3 bg-green-950 text-white font-semibold rounded-xl hover:bg-green-900 transition-colors disabled:opacity-50"
+            >
+              {loading ? <Loader2Icon className="animate-spin" /> : isLoginState ? "Sign In" : "Sign Up"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default login
+export default Login
