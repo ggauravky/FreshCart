5f7a1121240416df8d2e49d5dbc36140a6b97717
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-27 18:04:59 +0530

    Add promo banner and newsletter to home
    Introduce two new Home page sections: `AppPromoBanner` for app download promotion and `Newsletter` for email subscription signup. Both components are added under `client/src/components/Home/` and rendered in `Home.tsx` after `PopularProducts` to expand post-product engagement content.

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import App from "../App"
+import AppPromoBanner from "../components/Home/AppPromoBanner"
 import Features from "../components/Home/Features"
 import Hero from "../components/Home/Hero"
 import HomeCategories from "../components/Home/HomeCategories"
+import Newsletter from "../components/Home/Newsletter"
 import PopularProducts from "../components/Home/PopularProducts"
 
 const Home = () => {
@@ -10,6 +13,8 @@ const Home = () => {
       <Features />
       <HomeCategories />
       <PopularProducts />
+      <AppPromoBanner />
+      <Newsletter/>
     </div>
   )
 }
